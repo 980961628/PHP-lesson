c3f16de92663657a521740b325caa6b8fb1d827e
zhugege <980961628@qq.com>
2017-10-09 18:46:56 +0800

    GD库学习

--- 29.php
+++ 29.php
@@ -5,4 +5,53 @@
  * Date: 2017/10/9
  * Time: 16:22
  */
-    
+    /*
+     * GD 库
+     *
+     * 1.创建画布
+     * 2.准备颜色
+     * 3.填充画布
+     * 4.开始画
+     *
+     *  imagesetpixel       点
+     *  imageline           线
+     *  imagerectangle      矩形
+     *  imagefilledrectangle 填充矩形
+     *  imageellipse        圆
+     *  imagefilledellipse 填充圆
+     *  imagepolygon        多边形
+     *  imagefilledpolygon  填充多边形
+     *  imagettftext        常用  第一个字的左下角坐标
+     *
+     * 5.输出保存
+     * 6.释放资源
+     * */
+
+    $img = imagecreatetruecolor(300,200);
+
+    $color1 = imagecolorallocate($img,0,0,0);
+    $color2 = imagecolorallocate($img,255,0,0);
+    $color3 = imagecolorallocate($img,0,255,0);
+    $color4 = imagecolorallocate($img,0,0,255);
+    $color5 = imagecolorallocate($img,255,255,255);
+
+    imagefill($img,0,0,$color1);
+
+
+    imagesetpixel($img,10,10,$color5);
+    imagesetpixel($img,mt_rand(0,300),mt_rand(0,300),$color5);
+    imageline($img,10,10,100,100,$color4);
+    imagerectangle($img,100,100,50,50,$color3);
+    imagefilledrectangle($img,20,20,50,50,$color4);
+    imageellipse($img,200,50,50,50,$color5);
+    imagefilledellipse($img,200,100,50,50,$color5);
+    $arr = [250,0,10,150,490,150];
+    imagefilledpolygon($img,$arr,3,$color4);
+    imagestring($img,5,80,80,'AAAAAA',$color5);
+
+    imagettftext($img,20,0,150,20,$color5,'fonts/ygyxsziti2.0.ttf','你好啊');
+    header('content-type:image/jpeg');
+//    imagejpeg($img,'images/1.jpg');
+    imagejpeg($img);
+    imagedestroy($img);
+
--- README.md
+++ README.md
@@ -1,3 +1,32 @@
 # PHP-lesson
 PHP的学习课程
 云之梦PHP课程
+
+29 GD 库 20171009
+    1.创建画布
+    $img = imagecreatetruecolor(300,200);
+
+    2.准备颜色
+    $color1 = imagecolorallocate($img,0,0,0);
+
+    3.填充画布
+    imagefill($img,0,0,$color1);
+
+    4.开始画
+         *  imagesetpixel       点
+         *  imageline           线
+         *  imagerectangle      矩形
+         *  imagefilledrectangle 填充矩形
+         *  imageellipse        圆
+         *  imagefilledellipse 填充圆
+         *  imagepolygon        多边形
+         *  imagefilledpolygon  填充多边形
+         *  imagettftext        常用  第一个字的左下角坐标
+    5.输出保存
+    header('content-type:image/jpeg');
+    imagejpeg($img,'images/1.jpg');
+
+    6.释放资源
+    imagedestroy($img);
+
+
